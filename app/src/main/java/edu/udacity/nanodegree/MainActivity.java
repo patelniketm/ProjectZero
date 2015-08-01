f3fa2227077fd098e2ae84e70d7729d3448c9b81
Niket Patel <patelniket@gmail.com>
2015-07-31 21:47:23 -0500

    Project 0 Portfolio

---
+++ app/src/main/java/edu/udacity/nanodegree/MainActivity.java
@@ -0,0 +1,65 @@
+package edu.udacity.nanodegree;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onMovie(View v){
+        Toast.makeText(getApplicationContext(),"On Movies",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onScores(View v){
+        Toast.makeText(getApplicationContext(),"On Scores",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onLibrary(View v){
+        Toast.makeText(getApplicationContext(),"On Library",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onBuildItBigger(View v){
+        Toast.makeText(getApplicationContext(),"On BuildItBigger",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onBaconReader(View v){
+        Toast.makeText(getApplicationContext(),"On BaconReader",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onCapstone(View v){
+        Toast.makeText(getApplicationContext(),"On Capstone",Toast.LENGTH_SHORT).show();
+    }
+
+}
